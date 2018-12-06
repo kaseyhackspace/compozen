c6c14d201c23b21569c1253033db31c86a934802
Kasey Martin <kasey.hackspace@gmail.com>
2018-12-07 01:55:06 +0800

    added crud for ingredients; command for starting and stopping compozen

--- cli.js
+++ cli.js
@@ -8,11 +8,38 @@
 */
 
 const compozen_cli = require('commander');
-const { importRecipe, spinupRecipe } = require('./lib/compozen');
+const { 
+    compozenUp,
+    compozenDown,
+    importRecipe, 
+    spinupRecipe, 
+    addIngredient,
+    modIngredient,
+    delIngredient } = require('./lib/compozen');
 
 compozen_cli
     .version('0.0.1')
     .description('CLI tool to generate and manage docker-compose.yml files');
+
+compozen_cli
+    .command('up')
+    .alias('u')
+    .description('start compozen\'s redis server')
+    .action(
+        () => {
+            compozenUp();
+        }
+    );
+
+compozen_cli
+    .command('down')
+    .alias('d')
+    .description('stop compozen\'s redis server')
+    .action(
+        () => {
+            compozenDown();
+        }
+    );
 
 compozen_cli
     .command('import-recipe <recipe_name> <recipe_file>')
@@ -35,6 +62,35 @@ compozen_cli
         }
     );
 
+compozen_cli
+    .command('add-ingredient <recipe_name> <service_file>')
+    .alias('ai')
+    .description('import a YAML or JSON encoded docker-compose service into a recipe')
+    .action(
+        (recipe_name, service_file) => {
+            addIngredient({recipe_name, service_file});
+        }
+    );
+
+compozen_cli
+    .command('mod-ingredient <recipe_name> <service_name> <service_file>')
+    .alias('mi')
+    .description('modify a service in a recipe with a YAML or JSON encoded docker-compose service')
+    .action(
+        (recipe_name, service_name, service_file) => {
+            modIngredient({recipe_name, service_name, service_file});
+        }
+    );
+
+compozen_cli
+    .command('del-ingredient <recipe_name> <service_name>')
+    .alias('di')
+    .description('delete a service from a recipe')
+    .action(
+        (recipe_name, service_name) => {
+            delIngredient({recipe_name, service_name});
+        }
+    );
 
 compozen_cli.parse(process.argv);
 if(compozen_cli.args.length === 0){
